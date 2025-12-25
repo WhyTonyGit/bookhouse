8232ab83f0459c7c5b46b145efa04ce2c7d5cb8f
WhyTony <whytonycry@gmail.com>
2025-12-25 22:22:55 +0300

    UPD

--- src/db.py
+++ src/db.py
@@ -1,9 +1,7 @@
 # src/db.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-Base = declarative_base()
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 
 def get_database_url() -> str:
     return os.getenv(
@@ -11,9 +9,15 @@ def get_database_url() -> str:
         "postgresql+psycopg://postgres:postgres@localhost:5432/bookhouse",
     )
 
-def get_engine(url: str | None = None):
-    return create_engine(url or get_database_url(), pool_pre_ping=True)
+class Base(DeclarativeBase):
+    pass
 
-engine = get_engine()
-
+engine = create_engine(get_database_url(), pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
